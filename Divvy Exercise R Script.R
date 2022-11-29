d71f01cf9486082d0cd395e110eb9dc42caf44db
kamoamankwah <119394137+kamoamankwah@users.noreply.github.com>
2022-11-29 02:08:39 -0500

    Update Divvy Exercise R Script.R

--- Divvy Exercise R Script.R
+++ Divvy Exercise R Script.R
@@ -1,10 +1,3 @@
-# This analysis is based on the Divvy case study "'Sophisticated, Clear, and Polished’: 
-Divvy and Data Visualization" written by Kevin Hartman 
-(found here: https://artscience.blog/home/divvy-dataviz-case-study). 
-The purpose of this script is to consolidate downloaded Divvy data into a 
-single dataframe and then conduct simple analysis to help answer the key question: 
-“In what ways do members and casual riders use Divvy bikes differently?”
-
 # Install required packages
 # tidyverse for data import and wrangling
 # lubridate for date functions
